6e408f8c8e55aa8da7ba6ffd4f8c2649dfbf019f
MuznaAli30 <muznaalisiddiquisabzwari@gmail.com>
2024-10-14 22:37:17 +0500

    footer edit

--- Components/HeaderFooter/Footer2.js
+++ Components/HeaderFooter/Footer2.js
@@ -5,7 +5,7 @@ export default function Footer2() {
   return (
     <div className="bg-black p-16 bottom-0">
       <div className="flex justify-center items-center text-5xl mt-44 mb-16 text-white font-medium">
-        SOVY
+        DARBAR ONLINE
       </div>
       <div className="flex justify-around items-center mb-16">
         <div class="flex items-center gap-24 text-md font-medium text-[#d08829] max-lg:gap-10 max-lg:flex-col">
